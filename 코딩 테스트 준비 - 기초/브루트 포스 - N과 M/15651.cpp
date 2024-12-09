5b69ddf470253c6af624d3461f289da7925fb089
Seungho Jang <hoosong0235@gmail.com>
2024-12-09 14:38:09 +0000

    코딩 테스트 준비 - 기초/브루트 포스 - N과 M 1(8)

---
+++ 코딩 테스트 준비 - 기초/브루트 포스 - N과 M/15651.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(int n, int m, list<int> selecteds) {
+    if (!m) {
+        for (int selected: selecteds) cout << selected << ' ';
+        cout << '\n';
+        return;
+    }
+    
+    for (int i=1; i<=n; i++) {
+        selecteds.push_back(i);
+        solve(n, m - 1, selecteds);
+        selecteds.pop_back();       
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    int n, m; cin >> n >> m;
+    list<int> selecteds;
+    solve(n, m, selecteds);
+    
+    return 0;
+}
---
+++ 코딩 테스트 준비 - 기초/브루트 포스 - N과 M/15657.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(vector<int> is, int m, list<int> selecteds) {
+    if (!m) {
+        for (int selected: selecteds) cout << selected << ' ';
+        cout << '\n';
+        return;
+    }
+    
+    for (int i: is) {
+        if (*max_element(selecteds.begin(), selecteds.end()) <= i) {
+            selecteds.push_back(i);
+            solve(is, m - 1, selecteds);
+            selecteds.pop_back();
+        }
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    int n, m; cin >> n >> m;
+    vector<int> is(n); while (n--) cin >> is[n];
+    sort(is.begin(), is.end());
+    list<int> selecteds;
+    solve(is, m, selecteds);
+    
+    return 0;
+}
