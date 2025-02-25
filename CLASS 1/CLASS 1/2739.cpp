56a8784df9b259a80ed117bb337c86992453c07f
Seungho Jang <hoosong0235@gmail.com>
2025-02-25 23:54:59 +0000

    CLASS 1/CLASS 1 7(1)

---
+++ CLASS 1/CLASS 1/2739.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int n; cin >> n;
+    for (int m = 1; m <= 9; m++)
+        cout << n << " * " << m << " = " << n * m << '\n';
+}
